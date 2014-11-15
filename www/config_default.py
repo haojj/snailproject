7475bf78405a7051987dc04a353a8278998c054c
haojj <hjjxinji@163.com>
2014-11-15 19:36:03 +0800

    init create project

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Michael Liao'
+
+'''
+Default configurations.
+'''
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'test',
+        'password': '1234',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations.
+'''
+
+__author__ = 'kevinhao'
+
+configs = {
+    'db': {
+        'host': '119.29.6.213'
+    }
+}
